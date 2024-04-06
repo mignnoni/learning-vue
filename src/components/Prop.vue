15b120c9c4b3c2abd5de316038285b5670d48d3b
Iuri Mignoni <iurimig@gmail.com>
2024-04-06 11:43:58 -0300

    component with props

---
+++ src/components/Prop.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        msg: {
+            type: String,
+            required: true
+        }
+    }
+})
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+</template>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,14 +1,24 @@
 <template>
     <h2>Teste</h2>
+    <Prop :msg="message" />
     <button @click="navigate"></button>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
+import { defineComponent } from 'vue';
+import Prop from './Prop.vue';
     export default defineComponent({
+        components: {
+            Prop,
+        },
         methods: {
             navigate() {
                 this.$router.push('/')
+            }
+        },
+        data() {
+            return {
+                message: 'Hello World'
             }
         }
     })
